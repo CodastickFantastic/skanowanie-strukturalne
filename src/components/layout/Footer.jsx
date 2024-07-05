47938d9f1cae6ff5c0fb16ecd2ede004c35044b0
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2024-07-05 19:09:34 +0200

    Changed Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -18,7 +18,7 @@ export default function Footer() {
                     <Link href="https://www.facebook.com/profile.php?id=61560839454370">Facebook</Link>
                     <Link href="https://www.facebook.com/profile.php?id=61560839454370">Instagram</Link>
                 </div> */}
-                <p className={styles.logo}>Icons by: <Link href="https://icons8.com">Icons8</Link></p>
+                <p className={styles.logo}>Copyright © 2024 Skanowanie.pl Sp. z o.o. | All Rights Reserved | Icons by: <Link href="https://icons8.com">Icons8</Link></p>
             </div>
         </footer>
     )
